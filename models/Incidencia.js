78967c997a70ce779dad00d8963111d275872332
ii-v-vi <140362292+ii-v-vi@users.noreply.github.com>
2026-09-14 00:39:03 -0300

    Add files via upload

---
+++ models/Incidencia.js
@@ -0,0 +1,10 @@
+// ivan: define clase modelo incidencia
+class Incidencia {
+  constructor(id, tipo, descripcion) {
+    this.id = id;
+    this.tipo = tipo;
+    this.descripcion = descripcion;
+  }
+}
+
+module.exports = Incidencia;
